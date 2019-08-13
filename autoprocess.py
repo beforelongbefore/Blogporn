39f04956fea65a5e9a61a93ca638dd032a977f5a
beforelongbefore <41911311+beforelongbefore@users.noreply.github.com>
2019-08-13 18:46:08 +0800

    Add files via upload

--- autoprocess.py
+++ autoprocess.py
@@ -1,24 +1,102 @@
-import sys
 import csv
 import random
 import time
+import re
+import requests
+import urllib.request
+from contextlib import closing
+import os
 
-print('\n')
+
+#-----------------------------------------------------网页下载------------------------------------------------
+print('\n-----------------------------------------------选择文件----------------------------------------\n')
+url = r'http://10.75.1.151:50070/webhdfs/v1/complaint/mblog/?op=LISTSTATUS'
+res = urllib.request.urlopen(url)
+html1 = res.read().decode('utf-8')
+#print(html)
+files=re.findall(u'"pathSuffix":"(.*?)"',html1)
+#files
+print('已更新文件：')
+for i in range(len(files)):
+    print(str(i+1)+'.\t'+files[i])
+
+path=u'/Users/litian/PROGRAMMING/python/色情举报/数据/'
+myfiles=os.listdir(path)
+if len(myfiles)==0:
+    print('\n本地文件夹为空！')
+else:
+    print('\n已处理至：'+max(myfiles))
+
+
+choice=input('\n请选择要下载的文件：')
+day=files[int(choice)-1]
+
+
+os.mkdir(path+day)
+path=path+day
+print('\n创建文件夹：'+path)
+
+
+print('\n-----------------------------------------------下载文件---------------------------------------\n')
+print('开始下载文件: '+files[int(choice)-1])
+url2 = r'http://10.75.1.151:50070/webhdfs/v1/complaint/mblog/'+files[int(choice)-1]+'?op=LISTSTATUS'
+res = urllib.request.urlopen(url2)
+html2 = res.read().decode('utf-8')
+#print(html2)
+files2=re.findall(u'"pathSuffix":"(.*?)"',html2)
+originame=files2[1]
+#origname
+print('获取源文件名称：'+originame+'\n')
+
+
+
+#print("downloading with requests")
+url3 = 'http://10.75.1.151:50075/webhdfs/v1/complaint/mblog/'+files[int(choice)-1]+'/'+originame+'?op=OPEN&namenoderpcaddress=0.0.0.0:9000&offset=0' 
+
+
+#下载进度
+
+        
+#从网站下载下来的源文件，即后续处理的输入文件     files[int(choice)-1]+
+inname=path+'/'+originame
+        
+with closing(requests.get(url3, stream=True)) as response:
+    chunk_size = 1024 # 单次请求最大值
+    content_size = int(response.headers['content-length']) # 内容体总大小
+    with open(inname, "wb") as file:
+        mycount=1
+        for data in response.iter_content(chunk_size=chunk_size):
+            file.write(data)
+            #progress.refresh(count=len(data))
+            mycount = mycount+len(data)
+            old=0
+            if int(mycount/content_size*100)>old:
+                out='下载进度：'+str(int(mycount/content_size*100))+'%\t\t'+str(round(mycount/chunk_size,2))+' KB/ '+str(round(content_size/chunk_size,2))+' KB'
+                print('\r',out.ljust(40),end='',flush=True)
+                old=int(mycount/content_size*100)
+        print('下载成功！')
+
+
+
+
+#-----------------------------------------------处理excel-----------------------------
+
+print('\n-----------------------------------------------处理文件---------------------------------------\n')
 #fname=input('输入文件：')
-fname=str(sys.argv[1])
-f=open(fname,'r')
+#fname=str(sys.argv[1])#现在全自动处理，所以也不需要输入文件这个参数了
+f=open(inname,'r')
 line1=f.readline()
 date=line1.split('\t')[2].replace('\n','')
 count=len(f.readlines())+1#前面readline()使其减少了一行
 f.close()
-print('文件\t'+fname+'\n共\t'+str(count)+'条数据!'+'\n数据日期\t'+str(date))
+print('文件\t'+inname+'\n共\t'+str(count)+'条数据!'+'\n数据日期\t'+str(date))
 #outname=input('输出文件：')
-outname=u'/Users/litian/PROGRAMMING/f-porndata/Blogporn'+date.replace('-','')+u'.csv'
-print('输出路径：'+outname)
+outname=path+'/Blogporn'+date.replace('-','')+u'.csv'
+print('\n输出路径：'+outname+'\n')
 
 
 
-#主要代码
+
 if count>20000 and count<40000:
     n=count-20000
     #注意生成的随机数不能是重复的，否则失败
@@ -27,7 +105,7 @@ if count>20000 and count<40000:
     rmlist=random.sample(list,n)
     #rlist=rmlist
     
-    with open(fname,'r') as rf:
+    with open(inname,'r') as rf:
         lines=rf.readline()
         #lines=csv.reader(open(fname))
         with open(outname,'w') as f:
@@ -58,7 +136,7 @@ elif count>=40000:
     list=[i for i in range(count)]
     savelist=random.sample(list,20000)
     
-    with open(fname,'r') as rf:
+    with open(inname,'r') as rf:
         lines=rf.readline()
     
         with open(outname,'w') as f:
@@ -83,7 +161,7 @@ elif count>=40000:
                 
             
 else:
-     with open(fname,'r') as rf:
+     with open(inname,'r') as rf:
         lines=rf.readline()
         with open(outname,'w') as f:
             f.write(u'\ufeff')
@@ -103,4 +181,4 @@ else:
                 w.writerow(line)  
                 i=i+1
                 lines=rf.readline()
-print('\n随机抽样+分隔+BOM  处理完成!\n')
+print('\n\n随机抽样+分隔+BOM  处理完成!\n')
